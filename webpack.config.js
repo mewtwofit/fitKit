d870b6845f6f3fdb641bff1428ace2b7851842bd
Dean Ohashi <dnohashi@Deans-MacBook-Pro-2.local>
2019-06-14 17:58:19 -0700

    added files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const rules = [
+  {
+    test: /\.jsx?/,
+    exclude: /node_modules/, //aside from node_module, use babel-loader to load
+    loader: 'babel-loader'
+  },
+  {
+    test: /\.css$/,
+    use: ['style-loader', 'css-loader']
+  }
+];
+
+module.exports = {
+  target: 'web', //where to export code to
+  mode: 'development',
+  entry: __dirname + '/src/client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: { rules },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devServer: {
+    contentBase: './',
+    port: 5000
+  }
+};
