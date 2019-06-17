4ef9d6779a6bb513a8cb52413efadcb7327fa8c3
aaron <aaronmillersliveaccount@live.com>
2019-06-17 12:07:07 -0700

    React router added

--- client/components/App.js
+++ client/components/App.js
@@ -3,13 +3,63 @@ import Profile from "./Profile";
 import Exercise from "./Exercise";
 import Diet from "./Diet";
 import Summary from "./Summary";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Profile />
-      </div>
+<Router>
+<div>
+  <ul>
+    <li>
+      <Link to="/">Profile</Link>
+    </li>
+    <li>
+      <Link to="/exercise">Exercise</Link>
+    </li>
+    <li>
+      <Link to="/diet">Diet</Link>
+    </li>
+  </ul>
+
+  <hr />
+
+  <Route exact path="/" component={Profile} />
+  <Route path="/exercise" component={Exercise} />
+  <Route path="/diet" component={Diet} />
+</div>
+</Router>
     );
   }
 }
+
+
+
+
+{/* <div>
+<Profile /> 
+<Profile />
+</div> */}
+
+
+
+
+{/* <Router>
+<div>
+
+      <Link to="/">Profile</Link>
+
+      <Link to="/exercise">Exercise</Link>
+
+      <Link to="/diet">Diet</Link>
+
+
+  <hr />
+
+  <Route exact path="/" component={Profile} />
+  <Route path="/exercise" component={Exercise} />
+  <Route path="/diet" component={Diet} />
+</div>
+</Router> */}
+// );
+// }
+// }
