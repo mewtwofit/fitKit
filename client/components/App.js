174c994756b8af6962cb00d4a7c94366c3d63b02
Dean Ohashi <dnohashi@Deans-MacBook-Pro-2.local>
2019-06-17 15:25:51 -0700

    Data loading occurs in App.js now

--- client/components/App.js
+++ client/components/App.js
@@ -3,35 +3,74 @@ import Profile from "./Profile";
 import Exercise from "./Exercise";
 import Diet from "./Diet";
 import Summary from "./Summary";
+import { connect } from "react-redux";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-export default class App extends Component {
+import * as actions from "../actions/actions";
+
+class App extends Component {
+  componentDidMount() {
+    fetch("http://localhost:5000/getDiet")
+      .then(res => res.json())
+      .then(previousFoodData => {
+        for (let food of previousFoodData) {
+          this.props.addFoodAsync(food);
+        }
+      });
+
+    fetch("http://localhost:5000/getExercises")
+      .then(res => res.json())
+      .then(exercises => {
+        for (let exercise of exercises) {
+          this.props.addExerciseAsync(exercise);
+        }
+      });
+
+    fetch("http://localhost:5000/getUser")
+      .then(res => res.json())
+      .then(summaries => {
+        for (let summary of summaries) {
+          this.props.addSummaryAsync(summary);
+        }
+      });
+  }
+
   render() {
     return (
-<Router>
-<div>
-  <ul>
-    <li>
-      <Link to="/">Profile</Link>
-    </li>
-    <li>
-      <Link to="/exercise">Exercise</Link>
-    </li>
-    <li>
-      <Link to="/diet">Diet</Link>
-    </li>
-    <li>
-      <Link to="/summary">Summary</Link>
-    </li>
-  </ul>
-  <hr />
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Profile</Link>
+            </li>
+            <li>
+              <Link to="/exercise">Exercise</Link>
+            </li>
+            <li>
+              <Link to="/diet">Diet</Link>
+            </li>
+            <li>
+              <Link to="/summary">Summary</Link>
+            </li>
+          </ul>
+          <hr />
 
-  <Route exact path="/" component={Profile} />
-  <Route path="/exercise" component={Exercise} />
-  <Route path="/diet" component={Diet} />
-  <Route path="/summary" component={Summary} />
-</div>
-</Router>
-
+          <Route exact path="/" component={Profile} />
+          <Route path="/exercise" component={Exercise} />
+          <Route path="/diet" component={Diet} />
+          <Route path="/summary" component={Summary} />
+        </div>
+      </Router>
     );
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  addFoodAsync: foodData => dispatch(actions.addFoodAsync(foodData)),
+  addExerciseAsync: exercise => dispatch(actions.addExerciseAsync(exercise)),
+  addSummaryAsync: summary => dispatch(actions.addSummaryAsync(summary))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(App);
