17a52e3a278a5a40281a5b7edb3fd0291a7dd948
deanfchung <deanthechung@gmail.com>
2019-06-17 15:31:27 -0700

    created data chart

--- client/components/App.js
+++ client/components/App.js
@@ -4,6 +4,7 @@ import Exercise from "./Exercise";
 import Diet from "./Diet";
 import Summary from "./Summary";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Visualization from "./visualization";
 export default class App extends Component {
   render() {
     return (
@@ -19,6 +20,9 @@ export default class App extends Component {
     <li>
       <Link to="/diet">Diet</Link>
     </li>
+    <li>
+      <Link to="/visualization">Data</Link>
+    </li>
   </ul>
 
   <hr />
@@ -26,6 +30,7 @@ export default class App extends Component {
   <Route exact path="/" component={Profile} />
   <Route path="/exercise" component={Exercise} />
   <Route path="/diet" component={Diet} />
+  <Route path="/visualization" component={Visualization} />
 </div>
 </Router>
 
